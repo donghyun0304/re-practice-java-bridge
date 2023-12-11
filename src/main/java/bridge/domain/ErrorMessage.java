65fdb62e65da1ae25fc4bc8d09fe75cd6733ec0a
donghyunkim <ghffu405@gmail.com>
2023-12-11 23:16:27 +0900

    feat: 에러 메시지 추가

--- src/main/java/bridge/domain/ErrorMessage.java
+++ src/main/java/bridge/domain/ErrorMessage.java
@@ -3,5 +3,6 @@ package bridge.domain;
 public class ErrorMessage {
 
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
+    public static final String NOT_RANGE = "[ERROR] 범위안에 존재하지 않습니다.";
 
 }
