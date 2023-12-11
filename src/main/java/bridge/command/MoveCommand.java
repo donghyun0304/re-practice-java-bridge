4e47ec3c47a76c864715e7bffa71cca42e84833b
donghyunkim <ghffu405@gmail.com>
2023-12-11 23:45:50 +0900

    feat: MoveCommand 클래스 추가

---
+++ src/main/java/bridge/command/MoveCommand.java
@@ -0,0 +1,21 @@
+package bridge.command;
+
+import java.util.Arrays;
+
+public enum MoveCommand {
+    UP("U"),
+    DOWN("D");
+
+    private String name;
+
+    MoveCommand(String name) {
+        this.name = name;
+    }
+
+    public static MoveCommand findCommand(String moveCommand){
+        return Arrays.stream(values())
+                .filter(command -> command.name.equals(moveCommand))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] U 또는 D을 입력 해 주세요."));
+    }
+}
