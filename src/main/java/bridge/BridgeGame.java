22a6ef44cf2c1bc6dbf182e0b3bbb5d79ebffeb1
donghyunkim <ghffu405@gmail.com>
2023-12-12 01:08:30 +0900

    feat: retry 메소드 로직 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.command.BridgeMark;
 import bridge.command.MoveCommand;
+import bridge.command.TryCommand;
 import bridge.domain.Bridge;
 import bridge.domain.Result;
 
@@ -37,7 +38,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public Result move(MoveCommand moveCommand) {
+    public boolean move(MoveCommand moveCommand) {
         boolean ableMove = gameBridge.isAbleMove(moveCommand.getName(), this.moveCount);
         if(ableMove){
             if(moveCommand.getName().equals("U")){
@@ -48,6 +49,8 @@ public class BridgeGame {
                 upBridge.write(BridgeMark.EMPTY);
                 downBridge.write(BridgeMark.SUCCESS);
             }
+            this.moveCount++;
+            return true;
         }
         if(!ableMove){
             if(moveCommand.getName().equals("U")){
@@ -58,8 +61,13 @@ public class BridgeGame {
                 upBridge.write(BridgeMark.EMPTY);
                 downBridge.write(BridgeMark.FAIL);
             }
+            this.moveCount++;
+            return false;
         }
+        throw new IllegalStateException("[ERROR] 코드를 확인 해 주세요.");
+    }
 
+    public Result getResult(){
         return new Result(upBridge.getBridge(), downBridge.getBridge());
     }
 
@@ -68,6 +76,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(TryCommand tryCommand) {
+        if(tryCommand == TryCommand.RETRY){
+            return true;
+        }
+        if(tryCommand == TryCommand.END){
+            return false;
+        }
+        throw new IllegalStateException("[ERROR] 코드를 확인 해 주세요.");
     }
 }
