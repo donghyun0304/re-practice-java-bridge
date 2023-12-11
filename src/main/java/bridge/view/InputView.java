502b0fa6c2d1ea15474c828cfd036e6486e904bd
donghyunkim <ghffu405@gmail.com>
2023-12-11 23:16:10 +0900

    feat: 다리길이 입력 메소드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.util.RetryUtil;
+import bridge.validation.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +13,14 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return RetryUtil.getInput(() -> {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String input = Console.readLine();
+            //검증
+            InputValidator.validateOnlyNumber(input);
+            InputValidator.validateRangeNumber(Integer.parseInt(input));
+            return Integer.parseInt(input);
+        });
     }
 
     /**
