b01b033d4e93fb832dbecacc5199762d3993c848
donghyunkim <ghffu405@gmail.com>
2023-12-12 01:05:39 +0900

    feat: 재시도 여부 입력 메소드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.command.MoveCommand;
+import bridge.command.TryCommand;
 import bridge.util.RetryUtil;
 import bridge.validation.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
@@ -38,7 +39,11 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public TryCommand readGameCommand() {
+        return RetryUtil.getInput(() -> {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String input = Console.readLine();
+            return TryCommand.findCommand(input);
+        });
     }
 }
