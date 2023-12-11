b85d33df8f2c8ad78f3ccb0e6ad1a62448510830
donghyunkim <ghffu405@gmail.com>
2023-12-12 00:59:00 +0900

    feat: 최종결과 출력 메소드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -30,7 +30,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Result result) {
+        System.out.println("최종 게임 결과");
+        printMap(result);
     }
 
     public void printStart(){
@@ -56,5 +58,6 @@ public class OutputView {
         printRightVerticalBar();
         System.out.println();
     }
+
 
 }
